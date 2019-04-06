630b9fbebe993946cda8356502ffa2603e90d9d0
Alejandra <alemeque.am@gmail.com>
2019-04-06 09:36:40 -0400

    commit proyecto MVC

---
+++ MVCparcial1F/MVCparcial1F/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCparcial1F.MvcApplication" Language="C#" %>
---
+++ MVCparcial1F/MVCparcial1F/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCparcial1F.Startup))]
+namespace MVCparcial1F
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
